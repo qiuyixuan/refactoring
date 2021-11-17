0f935e02c838ee12eae2974e7290ebf598d15d83
Yixuan Qiu <yqiu22@colby.edu>
2021-11-16 23:37:34 -0500

    fix test_game

--- tests/test_game.py
+++ tests/test_game.py
@@ -5,7 +5,6 @@ Pong (Game) - test code
 """
 
 import sys
-
 sys.path.append("../after/")
 import class_based_pong_after as pong
 
@@ -20,4 +19,29 @@ def test_init():
     assert game.paddle_1.x_position == -350
     assert game.paddle_1.y_position == 0
     assert game.paddle_2.x_position == 350
-    assert game.paddle_2.y_position == 0
+
+    assert game.pen.shape() == "square"
+    assert game.pen.pencolor() == "white"
+    assert game.pen.shapesize()[0] == 1
+    assert game.pen.shapesize()[1] == 1
+    assert game.pen.pos() == (0.00, 260.00)
+    assert game.pen.isvisible() == False
+
+    assert game.border == 350
+
+def test_player_win():
+    game = pong.Game(800, 600, 350, 0)
+    game.player_win()
+
+    assert game.ball.x_position == 0
+    assert game.ball.y_position == 0
+    assert game.ball.ball_d["dx"] == -0.0925
+
+def test_collision():
+    game = pong.Game(800, 600, 350, 0)
+    game.collision(280)
+
+    assert game.ball.x_position == 280
+    assert game.ball.y_position == 0
+    assert game.ball.ball_d["dx"] == -1.5 * 0.0925
+    assert game.ball.ball_dx_initial == -0.0925
